98ee760cb95b9710776acb5270497f6199c623f8
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-20 01:03:25 +0300

    added view and some outlets and IBActions

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var turnLabel: UILabel!
+    
+    @IBOutlet var a1: UIButton!
+    @IBOutlet var a2: UIButton!
+    @IBOutlet var a3: UIButton!
+    
+    @IBOutlet var b1: UIButton!
+    @IBOutlet var b2: UIButton!
+    @IBOutlet var b3: UIButton!
+    
+    
+    @IBOutlet var c1: UIButton!
+    @IBOutlet var c2: UIButton!
+    @IBOutlet var c3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func boardTapAction(_ sender: Any) {
+    }
+    
 
 }
 
